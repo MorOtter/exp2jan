5443c81d159de669815966509e698dec615eb400
JK <jkmonkey515@gmail.com>
2025-01-17 12:08:59 +0800

    for publish

---
+++ routes/participantDetailsRoutes.js
@@ -0,0 +1,32 @@
+// imports
+const express = require("express");
+const setPacketArray = require("../public/scripts/packet");
+const {
+  setUpParticipant,
+  addFeedback,
+  addAccuracy,
+} = require("../controllers/participantDetailsController");
+
+// set router const
+const router = express.Router();
+
+router.post("/login", async (req, res) => {
+  const packetArray = await setPacketArray();
+  req.session.packetArray = packetArray;
+  setUpParticipant(req, res);
+});
+
+// get feedback view
+router.get("/feedback", (req, res) => {
+  res.render("feedback.ejs");
+});
+
+router.post("/feedback", async (req, res) => {
+  addFeedback(req, res);
+});
+
+router.post("/accuracy", async (req, res) => {
+  addAccuracy(req, res);
+});
+
+module.exports = router;
